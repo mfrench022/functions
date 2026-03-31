1f49c100e22ec1ed3ba28728c68fb23084f9588b
mfrench022 <mfrench022@gmail.com>
2026-03-31 18:31:06 -0400

    WORKING get placeholder alert pop up on amazon product page

---
+++ content.js
@@ -0,0 +1,6 @@
+// I used the following tutorials, along with Eric's recorded lecture, to develop the JSON file that identifies amazon product pages and displays the alert below:
+// https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world
+// https://developer.chrome.com/docs/extensions/develop/concepts/match-patterns
+// https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
+
+alert("Do you really want to buy this today?");
