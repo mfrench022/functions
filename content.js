4a3d9b937f72010d221ffa7b5fd3daf28b9af3f3
mfrench022 <mfrench022@gmail.com>
2026-04-01 11:43:47 -0400

    adding icons per tutorial, adding content js template from google

--- content.js
+++ content.js
@@ -3,7 +3,7 @@
 // https://developer.chrome.com/docs/extensions/develop/concepts/match-patterns
 // https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
 
-alert("Do you really want to buy this today?")
+// alert("Do you really want to buy this today?")
 
 // let productName = document.querySelector('#productTitle')
 
@@ -28,3 +28,31 @@ alert("Do you really want to buy this today?")
 // 		// And puts it into the page!
 // 		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
 // })
+
+
+function renderReadingTime(article) {
+  // If we weren't provided an article, we don't need to render anything.
+  if (!article) {
+    return;
+  }
+
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+renderReadingTime(document.querySelector("article"));
