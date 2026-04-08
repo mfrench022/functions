fdc712949ca287b8aa4cee1381e2598af56f2402
mfrench022 <mfrench022@gmail.com>
2026-04-08 09:07:44 -0400

    add padding from Amazon CSS

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// I used the following tutorials, along with Eric's recorded lecture, to develop the JSON file that identifies amazon product pages and displays the alert below:
+// I used the following tutorials, along with Eric's recorded lecture, to develop the JSON file that identifies amazon product pages and displays insert below:
 // https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world
 // https://developer.chrome.com/docs/extensions/develop/concepts/match-patterns
 // https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts
